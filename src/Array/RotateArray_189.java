e6e9005ad92d1e0ab22ddceca2b9341139a98b4f
Prerna Sharma <119722578+im-prerna@users.noreply.github.com>
2024-04-07 23:18:07 +0530

    first commit

---
+++ src/Array/RotateArray_189.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class RotateArray_189 {
+    public void rotate(int[] nums, int k) {
+        if(k==0) return;
+        if(nums==null || nums.length ==0) return;
+
+        int[] res = new int[nums.length];
+        for (int i = 0; i < nums.length ; i++) {
+            int newindex = (i+k) % nums.length;
+            res[newindex] = nums[i];
+        }
+        for (int i = 0; i < nums.length ; i++) {
+            nums[i] = res[i];
+
+        }
+    }
+}
